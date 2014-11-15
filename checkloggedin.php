9eaf858513fbf958e17b11a4701deac129a7d157
Fraser <Fraser@clearbooks.co.uk>
2014-11-15 20:28:54 +0000

    Remove error

--- checkloggedin.php
+++ checkloggedin.php
@@ -1,6 +1,6 @@
 <?php
 //Check if user is login
 if($user==""){
-die ("<meta http-equiv='REFRESH' content='0;url=/proj/co525/blog/fiar2/admin.php?error=You%20are%20not%20logged%20in!'>");
+die ("<meta http-equiv='REFRESH' content='0;url=/admin.php?error=You%20are%20not%20logged%20in!'>");
 }
 ?>
